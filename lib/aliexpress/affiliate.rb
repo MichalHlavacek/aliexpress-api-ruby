0b0b91824da45f4e70c40aab7b80836e8f80fce7
Chris Estreich <cestreich@gmail.com>
2017-10-09 17:55:48 -0700

    More tweaks

--- lib/aliexpress/affiliate.rb
+++ lib/aliexpress/affiliate.rb
@@ -4,7 +4,7 @@ module AliExpress
       'Apparel & Accessories' => 3,
       'Automobiles & Motorcycles' => 34,
       'Beauty & Health' => 66,
-      'Books for Local Russian' => 200004360,
+      'Books' => 200004360,
       'Computer & Office' => 7,
       'Home Improvement' => 13,
       'Consumer Electronics' => 44,
@@ -35,8 +35,8 @@ module AliExpress
     }.freeze
 
     FILTERS = %w(category keywords min_price max_price high_quality).freeze
-    SORTS   = %w(orignalPriceUp orignalPriceDown sellerRateUp sellerRateDown).freeze
-    FIELDS  = %w(productId productTitle productUrl imageUrl allImageUrls originalPrice salePrice localPrice discount volume).freeze
+    SORTS   = %w(orignalPriceUp orignalPriceDown volumeUp volumeDown sellerRateUp sellerRateDown).freeze
+    FIELDS  = %w(productId productTitle productUrl imageUrl allImageUrls originalPrice salePrice localPrice discount volume validTime storeName storeUrl).freeze
 
     SUCCESS = 20010000
     ERRORS  = {
@@ -50,13 +50,14 @@ module AliExpress
       20030060 => 'Fields input parameter error.',
       20030070 => 'Keyword input parameter error.',
       20030080 => 'Category ID input parameter error.',
+      20030090 => 'Tracking ID input parameter error.', # Buggy.
       20030140 => 'Page number input parameter error.',
       20030150 => 'Page size input parameter error (default = 20, max = 40).',
       20030160 => 'Sort input parameter error.'
     }.freeze
 
     class << self
-      def search(query:, filters: {}, sort: 'orignalPriceUp', page: nil, per_page: 40, fields: FIELDS)
+      def search(query: nil, filters: {}, sort: 'orignalPriceUp', page: nil, per_page: 40, fields: FIELDS)
         response = get(
           api_call: 'api.listPromotionProduct',
           params: {
